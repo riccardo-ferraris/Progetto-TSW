488144e42aadc8f0b1b723722d0ab84b548d4581
Nicola Frugieri <frugierinicola.ragg@gmail.com>
2022-06-15 10:18:27 +0200

    DB
    f

--- perspectiveArt.sql
+++ perspectiveArt.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `perspectiveart` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `perspectiveart`;
 -- MySQL dump 10.13  Distrib 8.0.27, for Win64 (x86_64)
 --
 -- Host: localhost    Database: perspectiveart
@@ -201,4 +203,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-06-15 10:10:25
+-- Dump completed on 2022-06-15 10:18:08
